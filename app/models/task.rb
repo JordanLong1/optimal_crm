8c9a6a1510b0bc9bd6424c006970a034eef98a85
Jordan <jordanleelong5@gmail.com>
2020-04-22 21:11:21 -0700

    getting things working

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
-    has_many :leads
-    has_many :salesrepresentatives, through: :leads 
+    belongs_to :sales_representative 
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "static#home"
 
-    resources :salesrepresentative, only: [:new, :create]
+    resources :salesrepresentatives, only: [:new, :create, :show]
 
   resources :organizations do 
     resources :leads, only: [:new, :create, :show, :edit, :destroy]
