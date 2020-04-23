c731e9a7dff5bea65f3e4e3c176454d398ead3f0
Jordan <jordanleelong5@gmail.com>
2020-04-22 18:58:13 -0700

    salesrep table created

--- db/migrate/20200422042053_create_salesrepresentatives.rb
+++ db/migrate/20200422042053_create_salesrepresentatives.rb
@@ -1,7 +1,9 @@
 class CreateSalesrepresentatives < ActiveRecord::Migration[6.0]
   def change
     create_table :salesrepresentatives do |t|
-
+      t.string :name
+      t.string :email 
+      t.string :password_digest
       t.timestamps
     end
   end
