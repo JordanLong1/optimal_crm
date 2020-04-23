044c81ff7c3cfce0cee6b37d62094a5b775e38cd
Jordan <jordanleelong5@gmail.com>
2020-04-22 19:08:31 -0700

    added attributes for tasks table

--- db/migrate/20200422042212_create_tasks.rb
+++ db/migrate/20200422042212_create_tasks.rb
@@ -1,7 +1,11 @@
 class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
-
+      t.string :title 
+      t.string :content 
+      t.boolean :complete
+      t.datetime :created_at 
+      t.datetime :updated_at 
       t.timestamps
     end
   end
