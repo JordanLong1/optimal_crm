8602f707aeffae415775132f8ab3d5ef2fb4dfc3
Jordan <jordanleelong5@gmail.com>
2020-04-22 19:00:22 -0700

    attributes for leads table

--- db/migrate/20200422042146_create_leads.rb
+++ db/migrate/20200422042146_create_leads.rb
@@ -1,7 +1,10 @@
 class CreateLeads < ActiveRecord::Migration[6.0]
   def change
     create_table :leads do |t|
-
+      t.string :name 
+      t.string :email 
+      t.string :phone_number 
+      t.boolean :customer 
       t.timestamps
     end
   end
