ad82e6503f7541c1e60cafb20760a54eea72f2c8
Jordan <jordanleelong5@gmail.com>
2020-04-22 19:02:17 -0700

    added attributes for organizations table

--- db/migrate/20200422042201_create_organizations.rb
+++ db/migrate/20200422042201_create_organizations.rb
@@ -1,7 +1,14 @@
 class CreateOrganizations < ActiveRecord::Migration[6.0]
   def change
     create_table :organizations do |t|
-
+      t.string :name 
+      t.string :revenue 
+      t.string :country 
+      t.string :city 
+      t.string :state 
+      t.string :address 
+      t.integer :user_id
+      t.boolean :customer
       t.timestamps
     end
   end
