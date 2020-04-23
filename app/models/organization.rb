e04890aa041dda250403dc2b3859fd0e2cc6d48f
Jordan <jordanleelong5@gmail.com>
2020-04-22 19:17:06 -0700

    added associations

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,2 +1,5 @@
 class Organization < ApplicationRecord
+
+    belongs_ to :salesrepresentative 
+    has_many :leads 
 end
--- app/models/salesrepresentative.rb
+++ app/models/salesrepresentative.rb
@@ -1,2 +1,5 @@
 class Salesrepresentative < ApplicationRecord
+    has_many :tasks 
+    has_many :organizations 
+    has_many :leads, through: :organizations 
 end
