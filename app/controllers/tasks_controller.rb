0c46593b8cd1cc4add81a4d90ffb0de3c95773d9
Jordan <jordanleelong5@gmail.com>
2020-04-23 20:53:12 -0700

    added find_and_set_task helper

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,13 @@
 class TasksController < ApplicationController 
+
+    def index 
+        @tasks = Task.all 
+    end
+
+    def show 
+        find_and_set_task
+    end
+
     def new 
         @task = Salesrepresentative.new 
     end
@@ -19,4 +28,10 @@ class TasksController < ApplicationController
         params.require(:task).permit(:title, :content, :complete)
  
     end
+
+    def find_and_set_task
+        @task = Task.find_by(id :params[:id])
+    end
+
+
 end
