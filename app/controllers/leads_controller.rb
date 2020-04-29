9c4d004f468bf542a26bc49e3ab5e4e35ddfa91b
Jordan <jordanleelong5@gmail.com>
2020-04-28 22:04:31 -0700

    connected salesrep to leads

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -9,37 +9,41 @@ class LeadsController < ApplicationController
     end
 
     def new 
-        @lead = Lead.new 
+        @lead = Lead.new(salesrepresentative_id: params[:salesrepresentative_id])
     end
 
     def edit 
-
+        find_and_set_lead
     end
     
     def create 
         @lead = Lead.new(lead_params)
         @lead.save
         redirect_to lead_path(@lead)
+
+    
     end
 
     def update 
-
+        find_and_set_lead
+        @lead.update(name: params[:lead][:name], email: params[:lead][:email], phone_number: params[:lead][:phone_number], customer: params[:lead][:customer])
+        redirect_to lead_path(@lead)
     end
 
     def destroy 
-
+        find_and_set_lead
     end
  
 
     private 
 
     def lead_params
-        params.require(:lead).permit(:name, :email, :phone_number, :customer)
+        params.require(:lead).permit(:name, :email, :phone_number, :customer, :salesrepresentative_id)
  
     end
 
     def find_and_set_lead
-        @lead = Lead.find_by(id: :params[:id])
+        @lead = Lead.find_by(id: params[:id])
     end
 
 
