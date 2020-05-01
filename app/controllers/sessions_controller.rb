98ee9268068f8c7be6d263a8e7d4cba491db479b
Jordan <jordanleelong5@gmail.com>
2020-04-30 21:44:25 -0700

    fixed create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,17 @@
 class SessionsController < ApplicationController 
 
     def new 
-        @session = Session.new 
+        @salesrep = Salesrepresentative.new 
     end
 
     def create 
-        if @salesrep = Salesrepresentative.new 
+        @salesrep = Salesrepresentative.find_by(email: params[:Salesrepresentative][:email])
             session[:id] = @salesrep.id 
-            redirect_to Salesrepresentative_path(@salesrep)
-        end
+            redirect_to salesrepresentative_path(@salesrep)
     end
 
     def destroy 
         session.delete :session_id
+        redirect_to root_path
     end
 end
