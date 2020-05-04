df04025c70fb4aebb42514c84744b3d39196429c
Jordan <jordanleelong5@gmail.com>
2020-05-03 19:33:15 -0700

    added current_user helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+    def current_user 
+        @current_user ||= Salesrepresentative.find_by(id: session[:salesrepresentative_id]) if session[:salesrepresentative_id]
+    end
 end
