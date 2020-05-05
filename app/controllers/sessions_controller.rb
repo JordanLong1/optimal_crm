d2bb858d03235202bba38a9bb2f1b8faa11c633a
Jordan <jordanleelong5@gmail.com>
2020-05-04 21:29:29 -0700

    added conditional to create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,12 @@ class SessionsController < ApplicationController
 
     def create 
         @salesrep = Salesrepresentative.find_by(email: params[:salesrepresentative][:email])
+        if @salesrep && @salesrep.authenticate(params[:salesrepresentative][:password])
             session[:salesrepresentative_id] = @salesrep.id 
             redirect_to salesrepresentative_path(@salesrep)
+        else 
+            redirect_to new_session_path, alert: "Couldn't find a user. Please try again"
+        end 
             # FOR ELSE redirect_to new_session_path, alert: "User not found. Please try again."
 
     end
