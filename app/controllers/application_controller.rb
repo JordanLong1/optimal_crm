903dbda24ab46f2420e24879137314649c3574e0
Jordan <jordanleelong5@gmail.com>
2020-05-04 21:27:49 -0700

    moving helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
+    # before_action :require_login
 
-    def logged_in?
-        !!session[:salesrepresentative_id]
 
-    end
+    private 
+
+   
+    # def require_login
+    #   unless logged_in?
+    #     flash[:error] = "You must be logged in to access this section"
+    #     redirect_to new_login_url # halts request cycl
+    #   end
+    # end
 
   
 end
