4229adc5f75ddc29bf4d06a7c98b5f47b99db66d
Jordan <jordanleelong5@gmail.com>
2020-05-04 21:30:40 -0700

    moving helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 module ApplicationHelper
+
+
+    def logged_in?
+        !!session[:salesrepresentative_id]
+
+    end
+
     def current_user 
         @current_user ||= Salesrepresentative.find_by(id: session[:salesrepresentative_id]) if session[:salesrepresentative_id]
     end
