2d83403e40238543ce69f44d599d39b72c8ffb57
Jordan <jordanleelong5@gmail.com>
2020-05-04 21:31:04 -0700

    added name validations

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -2,4 +2,7 @@ class Lead < ApplicationRecord
     
      belongs_to :organization, optional: true 
     belongs_to :salesrepresentative, optional: true
+
+    validates :name, presence: true 
+    validates :name, uniqueness: true 
 end
