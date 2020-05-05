50031ec3f4433b89944beb954acc67144358d5dd
Jordan <jordanleelong5@gmail.com>
2020-05-04 21:31:50 -0700

    added name validations

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,4 +2,8 @@ class Organization < ApplicationRecord
 
     has_many :leads
     has_many :salesrepresentatives, through: :leads
+
+
+    validates :name, presence: true 
+    validates :name, uniqueness: true 
 end
